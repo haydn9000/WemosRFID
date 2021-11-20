ee60d08473ec3e48572f240aaf167baae149c281
Haydn <haydnady@gmail.com>
2021-11-20 13:04:08 -0500

    Pin layout

---
+++ WemosRFID.ino
@@ -0,0 +1,13 @@
+
+/* Pin layout used:
+ * -----------------------------------------
+ *             MFRC522      Wemos       
+ *             Reader/PCD   D1 mini v2.2.0
+ * Signal      Pin          Pin
+ * -----------------------------------------
+ * RST/Reset   RST          D0
+ * SPI SS      SDA(SS)      D8
+ * SPI MOSI    MOSI         D7
+ * SPI MISO    MISO         D6
+ * SPI SCK     SCK          D5
+ */
