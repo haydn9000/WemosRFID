4021825b5a6212a38f921e2763ff07eeb130125f
Haydn <haydnady@gmail.com>
2022-01-03 18:34:56 -0500

    Formatting update

--- WemosRFID.ino
+++ WemosRFID.ino
@@ -5,7 +5,7 @@
  *             Reader/PCD   D1 mini v2.2.0
  * Signal      Pin          Pin
  * -----------------------------------------
- * RST/Reset   RST          D0
+ * RST/Reset   RST          D3
  * SPI SS      SDA(SS)      D8
  * SPI MOSI    MOSI         D7
  * SPI MISO    MISO         D6
@@ -15,54 +15,108 @@
 #include <SPI.h>
 #include <MFRC522.h>
 
-#define RST_PIN         D0          // Configurable, see typical pin layout above
-#define SS_PIN          D8         // Configurable, see typical pin layout above
+#define RST_PIN D0 // Configurable, see typical pin layout above
+#define SS_PIN D8  // Configurable, see typical pin layout above
 
-MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
+//MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
+MFRC522 rfid(SS_PIN, RST_PIN); // Instance of the class
 
-void setup() {
-  Serial.begin(74880);   // Initialize serial communications with the PC
-  delay(1000);
-  Serial.println("Setup");
-  
-  while (!Serial);    // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
-  SPI.begin();      // Init SPI bus
-  mfrc522.PCD_Init();   // Init MFRC522
-  mfrc522.PCD_DumpVersionToSerial();  // Show details of PCD - MFRC522 Card Reader details
-  Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
-  Serial.println("Setup done");
+MFRC522::MIFARE_Key key;
+
+// Init array that will store new NUID
+byte nuidPICC[4];
+
+void setup()
+{
+  Serial.begin(9600);
+  SPI.begin();     // Init SPI bus
+  rfid.PCD_Init(); // Init MFRC522
+
+  for (byte i = 0; i < 6; i++)
+  {
+    key.keyByte[i] = 0xFF;
+  }
+
+  Serial.println(F("This code scan the MIFARE Classsic NUID."));
+  Serial.print(F("Using the following key:"));
+  printHex(key.keyByte, MFRC522::MF_KEY_SIZE);
 }
 
-void loop() {
-//  Serial.println("Loop...");
-  
-  // Look for new cards
-  if ( ! mfrc522.PICC_IsNewCardPresent()) {
-    //delay(50);
+void loop()
+{
+
+  // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
+  if (!rfid.PICC_IsNewCardPresent())
+    return;
+
+  // Verify if the NUID has been readed
+  if (!rfid.PICC_ReadCardSerial())
+    return;
+
+  Serial.print(F("PICC type: "));
+  MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
+  Serial.println(rfid.PICC_GetTypeName(piccType));
+
+  // Check is the PICC of Classic MIFARE type
+  if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI &&
+      piccType != MFRC522::PICC_TYPE_MIFARE_1K &&
+      piccType != MFRC522::PICC_TYPE_MIFARE_4K)
+  {
+    Serial.println(F("Your tag is not of type MIFARE Classic."));
     return;
   }
 
-  // Select one of the cards
-  if ( ! mfrc522.PICC_ReadCardSerial()) {
-    //delay(50);
-    return;
+  if (rfid.uid.uidByte[0] != nuidPICC[0] ||
+      rfid.uid.uidByte[1] != nuidPICC[1] ||
+      rfid.uid.uidByte[2] != nuidPICC[2] ||
+      rfid.uid.uidByte[3] != nuidPICC[3])
+  {
+    Serial.println(F("A new card has been detected."));
+
+    // Store NUID into nuidPICC array
+    for (byte i = 0; i < 4; i++)
+    {
+      nuidPICC[i] = rfid.uid.uidByte[i];
+    }
+
+    Serial.println(F("The NUID tag is:"));
+    Serial.print(F("In hex: "));
+    printHex(rfid.uid.uidByte, rfid.uid.size);
+    Serial.println();
+    Serial.print(F("In dec: "));
+    printDec(rfid.uid.uidByte, rfid.uid.size);
+    Serial.println();
   }
+  else
+    Serial.println(F("Card read previously."));
 
-  // Show some details of the PICC (that is: the tag/card)
-  Serial.print(F("Card UID:"));
-  dump_byte_array(mfrc522.uid.uidByte, mfrc522.uid.size);
-  Serial.println();
-  
-  // Dump debug info about the card; PICC_HaltA() is automatically called
-  //mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
+  // Halt PICC
+  rfid.PICC_HaltA();
 
-  delay(1000);
+  // Stop encryption on PCD
+  rfid.PCD_StopCrypto1();
 }
 
-// Helper routine to dump a byte array as hex values to Serial
-void dump_byte_array(byte *buffer, byte bufferSize) {
-  for (byte i = 0; i < bufferSize; i++) {
+/**
+ * Helper routine to dump a byte array as hex values to Serial. 
+ */
+void printHex(byte *buffer, byte bufferSize)
+{
+  for (byte i = 0; i < bufferSize; i++)
+  {
     Serial.print(buffer[i] < 0x10 ? " 0" : " ");
     Serial.print(buffer[i], HEX);
   }
-} 
+}
+
+/**
+ * Helper routine to dump a byte array as dec values to Serial.
+ */
+void printDec(byte *buffer, byte bufferSize)
+{
+  for (byte i = 0; i < bufferSize; i++)
+  {
+    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+    Serial.print(buffer[i], DEC);
+  }
+}
